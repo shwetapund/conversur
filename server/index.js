2f6c9c1d1f7a8d088530b956bbed3149f1a49797
shwetapund <pundshweta2020@gmail.com>
2024-01-10 21:06:35 -0800

    socket io import and connection and create Get Api for send message

--- server/index.js
+++ server/index.js
@@ -3,9 +3,24 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import {signupApi, loginApi} from './controllers/UserAuth.js';
+import {Server} from 'socket.io';
 
 const app = express();
 app.use(express.json());
+
+const io = new Server(5002, {
+    cors: {
+        origin: '*'
+    },
+})
+
+io.on('connection',(socket) =>{
+    console.log('a user connected');
+
+    socket.on('message',(data)=>{
+        console.log(data);
+    })
+})
 
 const PORT = process.env.PORT || 5000;
 
@@ -27,6 +42,14 @@ app.get('/api/v1/health',async(req,res)=>{
 app.post('/api/v1/signup',signupApi)
 
 app.post('/api/v1/login',loginApi)
+
+app.get('/api/v1/sendMessage',(req,res)=>{
+    const {message} = req.query;
+    io.emit('receive',message);
+    res.json({
+        message:'message sent'
+    });
+})
 
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
